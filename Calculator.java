ab88f406b9e6478c0548b4d6722be77f77f7f44d
Sankumar02 <87709397+Sankumar02@users.noreply.github.com>
2022-06-16 20:28:33 +0530

    Add files via upload

---
+++ Calculator.java
@@ -0,0 +1,17 @@
+package com.Class.org;
+
+public class Calculator 
+{
+	static int number1=-5+8*6;	
+	static int number2=(55+9)%9;	
+	static int number3=20+-3*5/8;	
+	static int number4=5+15/3*2-8%3;
+	
+	public static void main(String[] args) 
+	{
+		System.out.println("Calculation of number1 = "+number1 );
+		System.out.println("\nCalculation of number2 = "+number2);
+		System.out.println("\nCalculation of number3 = "+number3);
+		System.out.println("\nCalculation of number4 = "+number4);
+	}
+}
